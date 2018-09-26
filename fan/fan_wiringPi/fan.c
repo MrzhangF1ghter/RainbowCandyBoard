32bd6f925d65fe61c1b48cc8ced0a6a53310c240
MrzhangF1ghter <651254084@qq.com>
2018-09-27 01:08:44 +0800

    添加风扇代码

--- fan/fan_wiringPi/fan.c
+++ fan/fan_wiringPi/fan.c
@@ -1,34 +1,1 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-
-#define TEMP_PATH "/sys/class/thermal/thermal_zone0/temp"
-#define MAX_SIZE 32
-int main(void)
-{
-	int fd;
-	double temp = 0;
-	char buf[MAX_SIZE];
-	while(1)
-	{
-		//open
-		fd = open(TEMP_PATH,O_RDONLY);
-		if(fd<0)
-		{
-			printf("failed to open /sys/class/thermal/thermal_zone0/temp\n");
-			return -1;	
-		}	
-		//read
-		if(read(fd,buf,MAX_SIZE)<0)
-		{
-			printf("failed to read temp\n");
-			return -1;
-		}
-		temp = atoi(buf)/1000.0;//convert to float;
-		printf("cpu temp :%.2f\n",temp);
-		close(fd);
-		sleep(1);
-	}
-}
+#include <stdio.h>#include <stdlib.h>#include <sys/types.h>#include <sys/stat.h>#include <fcntl.h>#include <unistd.h>#include <wiringPi.h>#define TEMP_PATH "/sys/class/thermal/thermal_zone0/temp"#define MAX_SIZE 32#define FAN 1//原生PWM引脚 GPIO18,wiringPi引脚为1int main(void){	int fd;	double temp = 0;	char buf[MAX_SIZE];	int speed;//速度值	int min_speed=10;//最小速度值	int temp_thre=40;//启动风扇的阈值	int speed_step=100;//步进值	if(wiringPiSetup()==-1)	{		printf("wiringPi gpio setup error\n");		return -1;	}	pinMode(FAN,PWM_OUTPUT);//配置为PWM输出	while(1)//循环检测	{		//open		fd = open(TEMP_PATH,O_RDONLY);//打开文件		if(fd<0)		{			printf("failed to open /sys/class/thermal/thermal_zone0/temp\n");			return -1;		}		//read		if(read(fd,buf,MAX_SIZE)<0)		{			printf("failed to read temp\n");			return -1;		}		temp = atoi(buf)/1000.0;//将读到的温度值转化为浮点数;		printf("cpu temp :%.2f\n",temp);		if(temp<=temp_thre)//如果CPU温度小于启动阈值		{			pwmWrite(FAN,0);//风扇转速为0		}		else if(temp>temp_thre) //如果CPU温度大于启动阈值		{			pwmWrite(FAN,1023);//全速运转10ms启动防卡转			usleep(10);			speed = min_speed+(temp-temp_thre)*speed_step;//速度=最小速度+温度差*速度步进值			if(speed>1023)//防止pwm溢出				speed=1023;			pwmWrite(FAN,speed);//设置pwm值		}		printf("speed:%d\n",speed);		close(fd);//关闭文件		sleep(5);//睡眠5s	}}
