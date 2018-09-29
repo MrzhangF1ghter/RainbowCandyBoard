6918f868d701071ce164bce176c0b0f91eb85c7e
MrzhangF1ghter <651254084@qq.com>
2018-09-29 15:25:47 +0800

    add uart

---
+++ uart/RaspberryPi3/wiringPi/UART.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <wiringPi.h>
+#include <wiringSerial.h>
+
+int main()
+{
+	int fd;
+	if(wiringPiSetup() < 0)return 1;
+//	if((fd = serialOpen("/dev/ttyAMA0",115200)) < 0)return 1;
+	if((fd = serialOpen("/dev/ttyS0",115200)) < 0)return 1;
+	printf("serial test start ...\n");
+	serialPrintf(fd,"Hello World!!!\n");
+	while(1)
+	{
+		serialPutchar(fd,serialGetchar(fd));
+	}
+	serialClose(fd);
+	return 0;
+}
